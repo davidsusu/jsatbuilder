5c01edf287394149ef119f1f96d7babb04bdb9a5
Dávid Horváth <horvath@werbarticum.hu>
2017-06-02 21:25:15 +0200

    Add some todo

--- jbooldep/src/hu/webarticum/jbooldep/Variable.java
+++ jbooldep/src/hu/webarticum/jbooldep/Variable.java
@@ -1,6 +1,38 @@
 package hu.webarticum.jbooldep;
 
 
+/*
+
+TPDP
+
+Evaluable
+Definition
+Brick
+Variable :Evaluable :Definition :Brick
+Composite :Brick
+Constraint :Composite
+Helper :Composite :Definition
+Expression :Evaluable
+Or: Expression
+And: Expression
+Not: Expression
+One: Expression [Restriction: Expression]
+
+##########
+
+new Constraint(required, dependencies, statement)
+new Helper(dependencies, statement)
+helper.getVariable()
+
+expressions / dependencies ??
+?? new <? extends Expression>(...evaluables[, some restrictions])
+
+Util.transformToCnf(expression[, variable])
+--> http://fmv.jku.at/limboole/
+--> https://github.com/dbasedow/aima-propositional-logic/blob/master/aima-core/src/main/java/aima/core/logic/propositional/visitors/ConvertToCNF.java
+
+*/
+
 public class Variable {
     
     // TODO
