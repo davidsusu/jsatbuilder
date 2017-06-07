9089a5bda4443a5949056adca8aa9c46def4e076
Dávid Horváth <horvath@werbarticum.hu>
2017-06-07 02:45:50 +0200

    Implement ConstraintSetSolverFiller

--- jsatbuilder/src/hu/webarticum/jsatbuilder/satbuilder/core/ConstraintSetSolverFiller.java
+++ jsatbuilder/src/hu/webarticum/jsatbuilder/satbuilder/core/ConstraintSetSolverFiller.java
@@ -1,23 +1,60 @@
 package hu.webarticum.jsatbuilder.satbuilder.core;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import hu.webarticum.jsatbuilder.sat.Solver;
+import hu.webarticum.jsatbuilder.satbuilder.core.Constraint;
+import hu.webarticum.jsatbuilder.satbuilder.core.Definition;
+import hu.webarticum.jsatbuilder.satbuilder.core.Dependant;
+import hu.webarticum.jsatbuilder.satbuilder.core.SolverFiller;
 
 public class ConstraintSetSolverFiller implements SolverFiller {
     
-    private List<Constraint> constraints = new ArrayList<Constraint>();
+    List<Constraint> constraints = new ArrayList<Constraint>();
     
     public void add(Constraint constraint) {
         constraints.add(constraint);
     }
-
+    
     @Override
     public void fillSolver(Solver solver) {
+        Set<Dependant> handledDependants = new HashSet<Dependant>();
+        Set<Dependant> unhandledDependants = new HashSet<Dependant>();
         
-        // TODO
+        for (Constraint constraint: constraints) {
+            if (!constraint.isRemoved()) {
+                unhandledDependants.add(constraint);
+            }
+        }
         
+        while (!unhandledDependants.isEmpty()) {
+            Set<Dependant> newDependants = new HashSet<Dependant>();
+            
+            for (Dependant dependant: unhandledDependants) {
+                for (Definition definition: dependant.getDependencies()) {
+                    if (
+                        definition instanceof Dependant &&
+                        !handledDependants.contains(definition) &&
+                        !unhandledDependants.contains(definition)
+                    ) {
+                        newDependants.add(dependant);
+                    }
+                }
+            }
+            
+            handledDependants.addAll(unhandledDependants);
+            
+            unhandledDependants = newDependants;
+        }
+        
+        for (Dependant dependant: handledDependants) {
+            if (dependant instanceof SolverFiller) {
+                ((SolverFiller)dependant).fillSolver(solver);
+            }
+        }
     }
-    
+
 }
