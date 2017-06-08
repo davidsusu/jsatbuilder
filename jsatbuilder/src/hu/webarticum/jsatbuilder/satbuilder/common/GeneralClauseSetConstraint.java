19f598d6a9205b0ad069af1a8bbcd07e87288bfc
Dávid Horváth <horvath@werbarticum.hu>
2017-06-08 07:03:06 +0200

    Add GeneralClauseSetConstraint

---
+++ jsatbuilder/src/hu/webarticum/jsatbuilder/satbuilder/common/GeneralClauseSetConstraint.java
@@ -0,0 +1,66 @@
+package hu.webarticum.jsatbuilder.satbuilder.common;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.IdentityHashMap;
+import java.util.List;
+import java.util.Map;
+
+import hu.webarticum.jsatbuilder.sat.Solver;
+import hu.webarticum.jsatbuilder.satbuilder.core.AbstractConstraint;
+import hu.webarticum.jsatbuilder.satbuilder.core.CollapseException;
+import hu.webarticum.jsatbuilder.satbuilder.core.Definition;
+
+public class GeneralClauseSetConstraint extends AbstractConstraint {
+    
+    private final List<List<DefinitionLiteral>> clauses;
+    
+    private final Definition condition;
+    
+    public GeneralClauseSetConstraint(boolean required, Collection<? extends Collection<DefinitionLiteral>> clauses, Definition condition) {
+        super(required);
+        this.clauses = new ArrayList<List<DefinitionLiteral>>();
+        for (Collection<DefinitionLiteral> clause: clauses) {
+            this.clauses.add(new ArrayList<DefinitionLiteral>(clause));
+        }
+        this.condition = condition;
+        for (Definition definition: getDependencies()) {
+            getDependencyManager().linkDependency(definition);
+        }
+    }
+
+    @Override
+    public List<Definition> getDependencies() {
+        Map<Definition, Object> dependencies = new IdentityHashMap<Definition, Object>();
+        for (List<DefinitionLiteral> clause: clauses) {
+            for (DefinitionLiteral literal: clause) {
+                dependencies.put(literal.getDefinition(), null);
+            }
+        }
+        if (condition != null) {
+            dependencies.put(condition, null);
+        }
+        return new ArrayList<Definition>(dependencies.keySet());
+    }
+
+    @Override
+    public void dependencyRemoved(Definition definition) throws CollapseException {
+        // XXX
+        remove();
+    }
+
+    @Override
+    public void fillSolver(Solver solver) {
+        for (List<DefinitionLiteral> clause: clauses) {
+            Solver.Clause solverClause = new Solver.Clause();
+            if (condition != null) {
+                solverClause.addLiteral(new Solver.Literal(condition, false));
+            }
+            for (DefinitionLiteral literal: clause) {
+                solverClause.addLiteral(literal.toSolverLiteral());
+            }
+            solver.add(solverClause);
+        }
+    }
+    
+}
